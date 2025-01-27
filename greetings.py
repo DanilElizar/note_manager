fdfc48dd7fb30a767d4b4a98db55e585f5d5b7ba
DanilElizar <danye3445@gmail.com>
2025-01-27 21:41:28 +0400

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,6 +1,15 @@
+from time import process_time
+
 username = 'Данил'
 title = 'Сходить в магазин'
 content = 'Купить сыр'
 status = 'Не выполнен'
 created_date = '26.01.2025'
 issue_date = '29.01.2025'
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date[:5])
+print(issue_date[:5])
